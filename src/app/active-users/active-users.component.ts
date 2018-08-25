5e0dee4fe663a6c564ca62396fc8862deb1966f7
yatoker <yusufandre@gmail.com>
2018-08-25 16:45:11 +0300

    Users service created

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component/*, EventEmitter, Input, Output*/ } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-active-users',
@@ -6,10 +7,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./active-users.component.css']
 })
 export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+  /*@Input()*/ users: string[];
+  //@Output() userSetToInactive = new EventEmitter<number>();
+
+  constructor(private usersService: UsersService){
+    this.users = this.usersService.activeUsers;
+  }
 
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.usersService.setToInactive(id);
+    //this.userSetToInactive.emit(id);
   }
 }
